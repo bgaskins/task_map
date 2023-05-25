903929e6504988f6d6f0fe79e27939a894760e7e
Bryan Gaskins <bryandgaskins@gmail.com>
2023-05-25 14:42:26 -0400

    Fixed queries/updated sql tables/rows

--- src/main/java/com/tasks/taskmap/repository/GoalRepository.java
+++ src/main/java/com/tasks/taskmap/repository/GoalRepository.java
@@ -16,9 +16,8 @@ public interface GoalRepository extends JpaRepository<Goal, Long>{
 	@Query(value = "SELECT * FROM goals", nativeQuery = true)
 	public Optional<Goal> getAllGoals();
 	
-	@Query(value = "INSERT INTO goals (goal_id, user_id, goal_title, destination_city, target_distance, timeframe) VALUES (?,?,?,?,?,?)", nativeQuery = true)
+	@Query(value = "INSERT INTO goals (goal_id, goal_id, goal_title, destination_city, target_distance, timeframe) VALUES (?,?,?,?,?,?)", nativeQuery = true)
 	public Optional<Goal> createGoal(@Param("goal_id") Long goalId,
-			@Param("user_id") String userId,
 			@Param("goal_title") String goalTitle,
 			@Param("destination_city") String destinationCity,
 			@Param("target_distance") String targetDistance,
@@ -28,7 +27,7 @@ public interface GoalRepository extends JpaRepository<Goal, Long>{
 	@Query(value = "UPDATE goals SET goal_title=?, destination_city=?, target_distance=?, timeframe=? WHERE id= :id", nativeQuery = true)
 	public Optional<Goal> updateGoal(@Param("id") Long id);
 	
-	@Query(value = "DELETE FROM goals WHERE user_id = :goalId", nativeQuery = true)
+	@Query(value = "DELETE FROM goals WHERE goal_id = :goalId", nativeQuery = true)
 	public Optional<Goal> deleteGoal(@Param("goalId") Long goalId);
 }
 
