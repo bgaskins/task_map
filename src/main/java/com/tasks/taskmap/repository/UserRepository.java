4b00e980d77b1abaedb0b02dc0ca323e16782937
Bryan Gaskins <bryandgaskins@gmail.com>
2023-05-25 14:39:48 -0400

    Fixed userId in queries

--- src/main/java/com/tasks/taskmap/repository/UserRepository.java
+++ src/main/java/com/tasks/taskmap/repository/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	List<User> findByUserId(Long userId);
 	
 	@Query(value = "INSERT INTO users (username, password, email) VALUES (?,?,?)", nativeQuery = true)
-	public Optional<User> createUser(@Param("goal_id") Long userId,
+	public Optional<User> createUser(@Param("user_id") Long userId,
 			@Param("username") String username,
 			@Param("password") String password,
 			@Param("email") String email
@@ -23,7 +23,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	@Query(value = "UPDATE users SET username=?, password=?, email=? WHERE user_id = :userId", nativeQuery = true)
 	public Optional<User> updateUser(@Param("userId") Long userId);
 	
-	@Query(value = "DELETE FROM users WHERE user_id = :goalId", nativeQuery = true)
+	@Query(value = "DELETE FROM users WHERE user_id = :userId", nativeQuery = true)
 	public Optional<User> deleteUser(@Param("userId") Long userId);
 
 
