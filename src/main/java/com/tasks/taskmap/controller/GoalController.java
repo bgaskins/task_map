b77f98c7547c0d043c70a1da81a88f9d38aa2997
Bryan Gaskins <bryandgaskins@gmail.com>
2023-06-02 12:31:32 -0400

    Annotation change

--- src/main/java/com/tasks/taskmap/controller/GoalController.java
+++ src/main/java/com/tasks/taskmap/controller/GoalController.java
@@ -3,6 +3,7 @@ package com.tasks.taskmap.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import com.tasks.taskmap.service.GoalService;
 
 @RestController
 @RequestMapping("/api/goals")
-public class GoalController {
+public class GoalController{
 	
 	@Autowired
 	private GoalService goalService;
